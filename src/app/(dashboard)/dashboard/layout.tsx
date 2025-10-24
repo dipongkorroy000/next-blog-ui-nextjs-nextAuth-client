0175cbf3e7f1cb5ced4c8b5792886d9bab18d778
dipongkorroy000 <dipongkorroy000@gmail.com>
2025-10-24 10:35:26 +0600

    update layout.tsx file

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,10 +1,6 @@
 import Sidebar from "@/components/shared/Sidebar";
 
-export default function DashboardLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <main className="min-h-dvh flex gap-4">
       <Sidebar />
